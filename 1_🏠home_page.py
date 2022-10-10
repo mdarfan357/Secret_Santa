dc7781a56c0784ca0747f00250a280f4ff96d44d
Mohammed Arfan <mdarfan357@gmail.com>
2022-10-10 09:27:24 +0530

    Add files via upload

---
+++ 1_🏠home_page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Multipage App",
+    page_icon = "🐐"
+)
+st.write("Hello world")
+st.title("This is the home page")
+# st.sidebar.success("select a page above.")
