e6df93ae3368e1a9d17db67d8be2af68ad416e9d
Mohammed Arfan <mdarfan357@gmail.com>
2022-10-11 11:46:51 +0530

    Update 3_☎️Developer_page.py

--- pages/3_☎️Developer_page.py
+++ pages/3_☎️Developer_page.py
@@ -1,31 +1,49 @@
 import streamlit as st
 import random 
 
-
-
 st.set_page_config(
     page_title = "Multipage App",
     page_icon = "🐐"
 )
-st.write("Hello World")
+
+if password == st.secrets.DEV_KEY:
+    tab_titles = ["Create Apps","Run Visualization charts","Display Outputs","Deploy the apps on the cloud"]
+
+    tabs = st.tabs(tab_titles)
+
+    with tabs[0]:
+        st.write("You are now in the create apps page")
+
+
+    with tabs[1]:
+        st.write("You are now in the charts page")
+
+    with tabs[2]:
+        st.write("You are now in the outputs page")
+
+
+    with tabs[3]:
+        st.write("You are now in the deployment page")
 
 
 
-if "Hello_World" not in st.session_state:
-        st.session_state["Hello_World"] = "Hello World!!"
+
+
+    if "Hello_World" not in st.session_state:
+            st.session_state["Hello_World"] = "Hello World!!"
 
 
 
-bt = st.button("Update session_state")
+    bt = st.button("Update session_state")
 
-if bt:
-    st.session_state.Hello_World = "Not Hello World"
+    if bt:
+        st.session_state.Hello_World = "Not Hello World"
 
-bt1 = st.button("Add session_state")
+    bt1 = st.button("Add session_state")
 
-if bt1:
-    st.session_state.x = random.randint(1,100)
-    
+    if bt1:
+        st.session_state.x = random.randint(1,100)
 
 
-st.write(st.session_state)
+
+    st.write(st.session_state)
