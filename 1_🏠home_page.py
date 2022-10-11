80bfaab8e272d8d15a2d12d375c9b70f14b5e916
Mohammed Arfan <mdarfan357@gmail.com>
2022-10-11 11:27:23 +0530

    Update 1_🏠home_page.py

--- 1_🏠home_page.py
+++ 1_🏠home_page.py
@@ -25,7 +25,7 @@ st.markdown("""
 st.markdown("<h1 style='text-align: center;'>secretBakra2.0</h1>", unsafe_allow_html=True)
 
 _left, mid, _right,r1,r2 = st.columns(5)
-with mid:
+with _right:
    st.image("https://media.tenor.com/IgO6EgqoAMkAAAAd/billy-goat-playing-guitar-playing-instrument.gif",width=400)
  
 
