0db8434a34b0898a3e6f63c33b4a27711e8fc7df
Mohammed Arfan <mdarfan357@gmail.com>
2022-10-11 11:04:16 +0530

    Update 2_😷Sign_up_page.py

--- pages/2_😷Sign_up_page.py
+++ pages/2_😷Sign_up_page.py
@@ -1,24 +1,41 @@
 import streamlit as st
-st.set_page_config(
-    page_title = "Multipage App",
-    page_icon = "🐐"
-)
-st.write("Hello World")
 
-tab_titles = ["Create Apps","Run Visualization charts","Display Outputs","Deploy the apps on the cloud"]
+st.title("Registration form")
+st.info("All * fields are mandatory")
 
-tabs = st.tabs(tab_titles)
+first,last = st.columns(2)
+first = first.text_input("First Name")
+last = last.text_input("Last Name")
+   
 
-with tabs[0]:
-    st.write("You are now in the create apps page")
-
-
-with tabs[1]:
-    st.write("You are now in the charts page")
+email,mob = st.columns([3,1])
+email = email.text_input("Email ID")
+mob = mob.text_input("Mob number")
+   
     
-with tabs[2]:
-    st.write("You are now in the outputs page")
-
+user,pw,pw2 = st.columns(3)
+user = user.text_input("Username*")
     
-with tabs[3]:
-    st.write("You are now in the deployment page")
+pw = pw.text_input("Password*",type="password")
+pw2 = pw2.text_input("Retype your password*",type="password")
+    
+    
+    
+ch,bl,sub = st.columns(3)
+submit = sub.button("Submit")
+
+
+if submit:
+   if ((user != "") or (pw != "") or (pw2 != "")):
+        if pw == pw2:
+            append_to_csv(first,last,email,mob,user,pw)
+            st.success("Success")
+            st.write("Please redirect to the login page...")
+            time.sleep(2)
+#                 pyautogui.hotkey('ctrl', 'f5')
+        else:
+            st.error("Passwords don't match")
+    else:
+        st.error("Please fill all the * credentials")
+
+st.write("""---""")
