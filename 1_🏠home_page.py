8327d58b908ff34156f23200bbd82abb3190dce1
Mohammed Arfan <mdarfan357@gmail.com>
2022-10-11 11:20:01 +0530

    Update 1_🏠home_page.py

--- 1_🏠home_page.py
+++ 1_🏠home_page.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="wide",
     page_icon = "🐐"
 )
-st.link("https://media.tenor.com/IgO6EgqoAMkAAAAd/billy-goat-playing-guitar-playing-instrument.gif")
+st.image("https://media.tenor.com/IgO6EgqoAMkAAAAd/billy-goat-playing-guitar-playing-instrument.gif")
 
 
 
